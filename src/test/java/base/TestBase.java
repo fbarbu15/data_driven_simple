90ba771961c96a2ef1ea14b415acddbd6177baf1
Florin Barbu <florin.barbu@oath.com>
2018-11-20 12:49:49 +0200

    add project structure and dependencies

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,30 @@
+package base;
+
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+	/*
+	 * WebDriver
+	 * Properties
+	 * Logs
+	 * ExtentReports
+	 * DB
+	 * Excel
+	 * Mail
+	 * 
+	 */
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		
+	}
+	
+}
