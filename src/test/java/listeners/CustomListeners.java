a5922f3dca187cad83a418d318d5383f336164cd
Florin Barbu <florin.barbu@oath.com>
2018-11-21 16:56:34 +0200

    add listeners

---
+++ src/test/java/listeners/CustomListeners.java
@@ -0,0 +1,48 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class CustomListeners implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onTestFailure(ITestResult result) {
+		
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		Reporter.log("Capturing screenshot");
+		Reporter.log("<a href=\"/Adtech/ProdPuma_2.png\">Screenshot</a>");
+
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
